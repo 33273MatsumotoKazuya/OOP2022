2fc3f7f36f9b86e09cf5dcb3ccc867f817e1275d
和也 松本 <33273@ojs.ac.jp>
2022-04-27 11:13:40 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+
+    public class Program {
+
+        static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom") {
+                //フィートからメートルへの対応表を出力
+                PrintMeterToFeet(1, 10);
+            }
+            else {
+                //メートルからフィートへの対応表を出力
+                PrintFeetToMeter(1, 10);
+            }
+        }
+
+        //フィートからメートルへの対応表を出力
+        static void PrintFeetToMeter(int start, int end) {
+            for (int feet = start; feet <= end; feet++) {
+                double meter = FeetConverter.FromMeter(feet);
+                Console.WriteLine("{0}ft = {1:0.0000}m", feet, meter);
+            }
+        }
+
+        //メートルからフィートへの対応表を出力
+        static void PrintMeterToFeet(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = FeetConverter.Tometer(meter);
+                Console.WriteLine("{0}m = {1:0.0000}ft", meter, feet);
+            }
+        }
+    }
+}
