686966ebdc2a78a2c8d7b706e514309be2ea0401
和也 松本 <33273@ojs.ac.jp>
2022-04-28 15:58:46 +0900

    varに変更（暗黙の型指定）

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -16,8 +16,8 @@ namespace SalesCalculator {
 
         //店舗別売り上げ
         public IDictionary<string, int> GetPerStoreSales() {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach (Sale sale in _sales) {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount;
                 else
@@ -28,11 +28,11 @@ namespace SalesCalculator {
 
         //売り上げデータを読み込み、Saleオブジェクトのリストを返す
         public static IEnumerable<Sale> ReadSales(string filePath) {
-            List<Sale> sales = new List<Sale>();
+            var sales = new List<Sale>();
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) {
                 string[] items = line.Split(',');
-                Sale sale = new Sale {
+                var sale = new Sale {
                     ShopName = items[0],
                     ProductCategory = items[1],
                     Amount = int.Parse(items[2])
