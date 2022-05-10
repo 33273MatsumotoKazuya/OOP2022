380226289143622a26ad4dd702038b50796c6515
和也 松本 <33273@ojs.ac.jp>
2022-05-10 13:38:42 +0900

    Songクラスを定義

---
+++ Chapter02/Exercise/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise {
+    class Song {
+
+        //歌のタイトル
+        public string Title { get; set; }
+
+        //アーティスト名
+        public string ArtistName { get; set; }
+
+        //演奏時間
+        public int Length { get; set; }
+    }
+}
