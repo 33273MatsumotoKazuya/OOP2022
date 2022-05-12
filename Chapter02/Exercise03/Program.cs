bc249d186ede3905f30ee848d6d345903dd78365
和也 松本 <33273@ojs.ac.jp>
2022-05-12 15:45:08 +0900

    問題2.3続き

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -7,21 +7,26 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("売上高の表示選択");
-            Console.WriteLine("1.店舗別");
-            Console.WriteLine("2.商品カテゴリー別");
-            Console.Write(">");
-            int judge = int.Parse(Console.ReadLine());
-            if (judge == 1) {
-                var sales = new SalesCounter("sales.csv");
-                var amountPerStore = sales.GetPerStoreSales();
-                foreach (var obj in amountPerStore) {
-                    Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+            int judge = 0;
+            var sales = new SalesCounter("sales.csv");
+
+            while (true) {
+                Console.WriteLine("売上高の表示選択（終了は999）");
+                Console.WriteLine("1.店舗別");
+                Console.WriteLine("2.商品カテゴリー別");
+                Console.Write(">");
+
+                judge = int.Parse(Console.ReadLine());
+                IDictionary<String, int> amountPerStore = null;
+                if (judge == 1) {
+                    amountPerStore = sales.GetPerStoreSales();
                 }
-            }
-            else if(judge == 2) {
-                var sales = new SalesCounter("sales.csv");
-                var amountPerStore = sales.GetPerProductSales();
+                else if (judge == 2) {
+                    amountPerStore = sales.GetPerProductSales();
+                }
+                else if (judge == 999) {
+                    break;
+                }
                 foreach (var obj in amountPerStore) {
                     Console.WriteLine("{0}{1}", obj.Key, obj.Value);
                 }
