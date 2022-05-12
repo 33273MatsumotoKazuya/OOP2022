358a54d1eb385efe2a744728b5c8b8be0f2b92d1
和也 松本 <33273@ojs.ac.jp>
2022-05-12 15:09:29 +0900

    問題2.3の完成

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -7,6 +7,25 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("売上高の表示選択");
+            Console.WriteLine("1.店舗別");
+            Console.WriteLine("2.商品カテゴリー別");
+            Console.Write(">");
+            int judge = int.Parse(Console.ReadLine());
+            if (judge == 1) {
+                var sales = new SalesCounter("sales.csv");
+                var amountPerStore = sales.GetPerStoreSales();
+                foreach (var obj in amountPerStore) {
+                    Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+                }
+            }
+            else if(judge == 2) {
+                var sales = new SalesCounter("sales.csv");
+                var amountPerStore = sales.GetPerProductSales();
+                foreach (var obj in amountPerStore) {
+                    Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+                }
+            }
         }
     }
 }
---
+++ Chapter02/Exercise03/SalesCounter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    public class SalesCounter {
+        private IEnumerable<Sale> _sales;   //csvファイルから読み込んだデータ
+
+        //コンストラクタ
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
+        }
+
+        //店舗別売り上げ
+        public IDictionary<string, int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    dict[sale.ShopName] = sale.Amount;
+            }
+            return dict;
+        }
+
+        //商品別売り上げ
+        public IDictionary<string, int> GetPerProductSales() {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.ProductCategory))
+                    dict[sale.ProductCategory] += sale.Amount;
+                else
+                    dict[sale.ProductCategory] = sale.Amount;
+            }
+            return dict;
+        }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static IEnumerable<Sale> ReadSales(string filePath) {
+            var sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                string[] items = line.Split(',');
+                var sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+    }
+}
