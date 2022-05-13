db26bca6047d68cfe47c243103bf059736b2e977
和也 松本 <33273@ojs.ac.jp>
2022-05-13 15:18:07 +0900

    p90まで

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            var name = list.FindAll(s => s.Length <= 5);
+            Console.WriteLine(name);
+        }
+    }
+}
