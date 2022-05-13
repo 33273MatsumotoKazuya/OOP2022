068adf074759c242ff21c8d4b10bec3e01bb9525
和也 松本 <33273@ojs.ac.jp>
2022-05-13 15:27:08 +0900

    問題3.1.1の完成

---
+++ Chapter03/Exercise01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            var exists = numbers.Exists(s => s % 8 == 0 || s % 9 == 0);
+            Console.WriteLine(exists);
+        }
+    }
+}
