6aa70c4821fcd4cb0c92889030ae44dfc8c6eb07
和也 松本 <33273@ojs.ac.jp>
2022-05-24 13:28:46 +0900

    Exercise02の追加

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Exercise01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01 {
+namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
             var ym = new YearMonth(2002, 11);
