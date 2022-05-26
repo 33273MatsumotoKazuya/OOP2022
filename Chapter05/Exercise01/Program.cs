5265577473e4343a00e05b00ef716a29d7623b82
和也 松本 <33273@ojs.ac.jp>
2022-05-26 15:47:59 +0900

    問題5.3.2まで

--- Chapter05/Exercise01/Program.cs
+++ Chapter05/Exercise01/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+
+            //5.1
+            Console.Write("文字列1：");
+            var str1 = Console.ReadLine();
+            Console.Write("文字列2：");
+            var str2 = Console.ReadLine();
+            if (String.Compare(str1, str2, ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+                Console.WriteLine("-------------");
+            } else {
+                Console.WriteLine("等しくない");
+                Console.WriteLine("-------------");
+            }
         }
     }
 }
---
+++ Chapter05/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("数字文字列：");
+            var strNum = Console.ReadLine();
+            if (int.TryParse(strNum, out var result)) {
+                Console.WriteLine("{0:N0}", result);
+            } else {
+                Console.WriteLine("不適な数字文字列です");
+            }
+        }
+    }
+}
