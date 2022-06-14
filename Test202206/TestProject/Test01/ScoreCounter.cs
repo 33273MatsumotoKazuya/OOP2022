b0cb7ce62184f3cde5c05d2a23082ff385bbf956
和也 松本 <33273@ojs.ac.jp>
2022-06-14 15:33:02 +0900

    統一試験　コメント追加

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -11,7 +11,7 @@ namespace Test01 {
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要： 生徒データを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
             var students = new List<Student>();
             string[] lines = File.ReadAllLines(filePath);
@@ -27,7 +27,7 @@ namespace Test01 {
             return students;
         }
 
-        //メソッドの概要： 
+        //メソッドの概要：　科目別の得点合計を求める
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
             foreach (var score in _scores) {
--- Test202206/TestProject/Test01/Student.cs
+++ Test202206/TestProject/Test01/Student.cs
@@ -1,7 +1,11 @@
 ﻿namespace Test01 {
+    //生徒クラス
     class Student {
+        //名前
         public string Name { get; set; }
+        //教科科目
         public string Subject { get; set; }
+        //得点
         public int Score { get; set; }
     }
 }
